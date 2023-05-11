3f262f494d2929c7b2c88b11bad33c731bb90bad
Imran <m.imranshahab@gmail.com>
2023-05-11 14:16:12 -0400

    TopTab and Block

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Drawer from './components/Drawer';
 import Header from './components/Header';
+import Block from './components/Block';
+import TopTab from './components/TopTab';
 
 function App() {
   return (
     <div>
       <Drawer isOpen={true} />
       <Header/>
+      <TopTab/>
+      <Block/>
     </div>
   );
 }
