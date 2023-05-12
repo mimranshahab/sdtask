24c9afaf7b91c22ded5116f5ab5091668c5dda80
Imran <m.imranshahab@gmail.com>
2023-05-12 00:26:36 -0400

    calculator-logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Drawer from './components/Drawer';
 import Header from './components/Header';
+import CalculatorDark from './components/calculator-dark';
 
 function App() {
   return (
     <div>
-      <Drawer isOpen={true} />
-      <Header/>
+      {/* <Drawer isOpen={true} />
+      <Header/> */}
+      <CalculatorDark/>
     </div>
   );
 }
